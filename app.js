a2680a8fb1e01a629a0917b4535eb1fda3d8c4d3
AngelicaBM <gbedoya@summa-sci.com>
2022-06-25 00:14:20 -0500

    agrego carpetas, empiezo app.js y Header index.html

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("gran proyecto");
+// requerimos express y path
+const express = require('express')
+const path= require('path');
+
+// guardamos en un constante app la funcionalidad de express()
+const app = express()
+
+// definimos el puerto en el que se va a levantar el servidor
+const port = 3000
+
+// definimos las rutas para los archivos estáticos(públicos) y otra para las vistas
+const viewsPath = path.join(__dirname, 'views');
+const publicPath = path.join(__dirname, 'public');
+
+// Agregamos el middleware para configurar la carpeta de archivos estáticos
+app.use(express.static(publicPath));
+
+// Definimos la ruta que responda a GET "/" con la vista home.html
+app.get('/', (req, res) => res.sendFile(path.join(viewsPath, 'index.html')));
+
+// Levantamos el servidor con app.listen(port)
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}!`))
