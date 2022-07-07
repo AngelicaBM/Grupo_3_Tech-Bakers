0085382fd284adedfac57728fba6f135e3ec2e44
Ezeiac <ezearena@gmail.com>
2022-07-07 08:35:32 -0300

    .

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.static(publicPath));
 app.get('/', (req, res) => res.sendFile(path.join(viewsPath, 'index.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(viewsPath, 'register.html')));
 app.get('/login', (req, res) => res.sendFile(path.join(viewsPath, 'login.html')));
+app.get('/carrito', (req, res) => res.sendFile(path.join(viewsPath, 'carrito.html')));
 
 // Levantamos el servidor con app.listen(port)
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}!`))
