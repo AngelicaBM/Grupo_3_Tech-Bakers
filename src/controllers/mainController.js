c0ac4b0994322123d6449c5ab53934fcea831194
Ezeiac <ezearena@gmail.com>
2022-07-30 13:32:02 -0300

    multer - model

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,16 +1,23 @@
-const fs = require('fs');
-const path = require('path');
-/* const products = require('../dataBase/products.js'); */
+const jsonDB = require('../model/jsonDatabase');
 const nosotros = require('../dataBase/nosotros.js');
-const productsJSON = fs.readFileSync(path.resolve(__dirname, '../dataBase/products.json'), 'utf8');
-const products = JSON.parse(productsJSON);
-
+const productModel = jsonDB('products')
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const mainController = {
     index : (req,res)=>{
-        const destacados = products.filter(product => product.category === "Destacados");
-        res.render('products/index', {nosotros, destacados})
+		
+        const destacados = productModel.destacados('Destacados')
+        res.render('products/index', { nosotros, destacados,toThousand})
     },
 
+    search: (req, res) => {
+		let search = req.query.keywords;
+		let productsToSearch = products.filter(product => product.name.toLowerCase().includes(search));	
+		res.render('results', { 
+			products: productsToSearch, 
+			search,
+			toThousand,
+		});
+	},
 }
 
 module.exports = mainController;
---
+++ src/model/jsonDatabase.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const path = require('path');
+// Recibo por parámetro la entidad para reutilizarlo
+
+
+const modelController = function (name) {
+
+    return {
+        tablePath: path.resolve(__dirname, '../dataBase/', `${name}.json`),
+
+        // Leo el archivo Json y lo transformo en Array de objeto literal     
+        readFile: function () {
+            let tableContents = fs.readFileSync(this.tablePath, 'utf-8');
+            return JSON.parse(tableContents) || [];
+        },
+        // Grabo el array que recibo por parámetro y lo paso a formato Json
+        writeFile: function (contents) {
+            let tableContents = JSON.stringify(contents, null, ' ');
+            fs.writeFileSync(this.tablePath, tableContents);
+        },
+        // Averiguo el próximo id
+        nextId: function () {
+            let rows = this.readFile();
+            let lastRow = rows.pop();
+
+            return lastRow.id ? ++lastRow.id : 1;
+        },
+        // Leo todos los registros del archivo
+        all: function () {
+            
+            return this.readFile();
+        },
+        // Busco por id
+        find: function (id) {
+            let rows = this.readFile();
+            return rows.find(i => i.id == id);
+        },
+
+        // agrego un registro que paso por parámetro
+        create: function (row) {
+            let rows = this.readFile();
+            // Averiguo el último id y lo actualizo
+            row.id = this.nextId();
+            // Agrego en el array
+            rows.push(row);
+            // grabo el array en el archivo
+            this.writeFile(rows);
+            //Retorno el último id generado
+            return row.id;
+        },
+        // Actualizo el archivo
+        update: function (row) {
+            let rows = this.readFile();
+
+            let updatedRows = rows.map(oneRow => {
+                if (oneRow.id == row.id) {
+                    return row;
+                }
+
+                return oneRow;
+            });
+            this.writeFile(updatedRows);
+
+            return row.id;
+        },
+
+        // Elimino el registro en el archivo según un id    
+        delete: function (id) {
+
+            let rows = this.readFile();
+            let updatedRows = rows.filter(row => {
+                return row.id != id;
+            });
+
+            this.writeFile(updatedRows);
+        },
+
+        enOferta: function () {
+            let rows = this.readFile();
+  
+            const visitados = rows.filter(i => i.category == 'En oferta')
+            return visitados 
+
+        },
+
+        destacados: function () {
+            let rows = this.readFile();
+  
+            const enVenta = rows.filter(i => i.category == 'Destacados')
+            return enVenta
+
+        },
+
+        findFirstByField: function(text){
+            let rows = this.all();
+    
+            let elementFound = rows.find(element => element.id == text);
+            return elementFound;
+        },
+
+        findAllByField: function(text){
+            let rows = this.all();
+            let allElementsFound = rows.filter(element => element.type == text);
+            return allElementsFound;
+        }
+
+
+
+       
+
+
+
+    }
+}
+
+module.exports = modelController
