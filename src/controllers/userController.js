87044959dcc6bc2b2a170a9c20064a57198bfbcc
Ezeiac <ezearena@gmail.com>
2022-08-02 13:34:58 -0300

    heroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,24 +6,6 @@ const userController = {
     register : (req,res)=>{
         res.render('users/register')
     },
-    create : (req,res)=>{
-        res.render('products/create');
-    },
-    
-    store : (req, res) => {
-		let imagenes= []
-
-        for(let i = 0 ; i<req.files.length;i++){
-            imagenes.push(req.files[i].filename)
-        }
-        console.log(req.files)
-		const newProduct = {
-			...req.body,
-			image: req.files.length >= 1  ? imagenes : ["default-image.png"]
-		}
-		productModel.create(newProduct)
-		res.redirect('/')
-	},
 }
 
 module.exports = userController;
