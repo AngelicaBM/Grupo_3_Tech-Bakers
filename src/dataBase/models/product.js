90444d25627c83ce8f9e7828a3440dc2512decb6
Angelica Bedoya Montoya <gbedoya@summa-sci.com>
2022-09-04 19:54:02 -0500

    Instalacion sequelize

---
+++ src/dataBase/models/product.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    typesId: DataTypes.INTEGER,
+    price: DataTypes.DECIMAL,
+    discount: DataTypes.DECIMAL,
+    description: DataTypes.TEXT,
+    stock: DataTypes.INTEGER,
+    categoriesId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
