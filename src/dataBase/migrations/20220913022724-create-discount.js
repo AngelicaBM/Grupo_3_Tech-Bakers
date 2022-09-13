ee6cfd26b40368f1895936bd32032c87d1d9fcfd
Chino89 <im.emm@live.com>
2022-09-12 23:37:41 -0300

    voucher db

---
+++ src/dataBase/migrations/20220913022724-create-discount.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('discounts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      users_id: {
+        type: Sequelize.INTEGER
+      },
+      voucher_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('discounts');
+  }
+};
