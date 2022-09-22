67a9fddf66acdab29c27b1ae7d8a788e2a47fd55
Angelica Bedoya Montoya <gbedoya@summa-sci.com>
2022-09-22 11:12:39 -0500

    register user db

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const authMiddleware = (req, res, next) => {
     
-    if(!req.session.usuarioLogueado){
+    if(!req.session.usuarioLogueado && req.session.userLogged.roleId == 1){
         return res.redirect("/users/login");
     }
    
