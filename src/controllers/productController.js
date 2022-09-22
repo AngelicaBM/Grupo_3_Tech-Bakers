5849313d7b60283282655495d396a178dfdfbd17
Angelica Bedoya Montoya <gbedoya@summa-sci.com>
2022-09-22 06:31:30 -0500

    productedit

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,6 +6,7 @@ const universalModel = require('../model/universalModel.js');
 const productModel = universalModel ('products')
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const { Image, sequelize } = require("../dataBase/models");
+const { where } = require('sequelize');
 
 const productController = {
     productCart : (req,res)=>{
@@ -73,7 +74,7 @@ const productController = {
                 };
                 const categories = await db.Category.findAll();
                 const types = await db.Type.findAll();
-                res.render('products/create',{errors: errores.mapped(), oldData: req.body,types,categories});
+                res.render('products/edit',{errors: errores.mapped(), oldData: req.body,types,categories});
             }
         } catch (error) {
             res.json({error: error.message});
@@ -96,80 +97,24 @@ const productController = {
 },
 
 update: (req, res) => {
-        const errores = validationResult(req);  
-        if (errores.isEmpty()) {
-          //asi que simplmente tomare todos los datos que vienen y hare el update si los campos estan correctos
-          let idToUpdate = req.params.id;
-          //busco la imagen que este vinculada al producto a editar para usarlo luego
-          let image = db.Image.findOne({
-            where: {
-              productId: idToUpdate,
-            },
-          }).then(() => {
-            //verifico si se eligio una nueva imagen o no
-            if (req.file == undefined) {
-              //en el caso de que no se haya elegido una nueva imagen, se usara la que ya estaba vinculada
-              db.Image.update({
-                name : image.name,
-            },{
-                where : {
-                    productId : idToUpdate
-                }
-            })
-            } else {
-              //en caso de elegir una nueva imagen se tomara esa y se hara el update de la misma, antes de que se haga
-              //el update de los campos del body
-              db.Image.update({
-                name : req.file.filenamee,
-            },{
-                where : {
-                    productId : idToUpdate
-                }
-            })
-              imageController.edit(productId, req.file.filename);
+
+        db.Product.update({
+            
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.descripcion,
+            discount: req.body.discount,
+            stock: req.body.stock,
+            categoryId: req.body.category,
+            typeId: req.body.type,
+        },{
+            where:{
+                id: req.params.id
             }
-            //update del body
-            db.Product.update(
-              {
-                name: req.body.name,
-                typeId: req.body.ype,
-                price: req.body.price,
-                description: req.body.description,
-                stock: req.body.stock,
-                categoryId: req.body.category,
-                discount: req.body.discount,
-              },
-              {
-                where: {
-                  id: idToUpdate,
-                },
-              }
-            ).then(() => {
-              return res.redirect(`/products/productDetails/${req.params.id}`);
-            });
-          });
-
-        } else {
-            //en caso de que hayan errores en las validaciones requerimos todos los datos del producto
-            let id = req.params.id;
-            let product = db.Product.findByPk(id);
-            let categories = db.Category.findAll();
-            let types = db.Type.findAll();
-            //procedemos a cargarlas junto al renderizado de la vista de editar, ademas tambien pasamos los errores
-            //y los datos previos para que estos no se pierdan
-            Promise.all([product,categories,types,]
-              ).then(
-              ([
-                product,categories,types,
-              ]) => {
-                res.render("products/edit", {
-                  product,categories,types,errors: errores.mapped(),oldData: req.body,
-                });
-              }
-            );
-          }
-        },
-
+        });
+             res.redirect('/')
+    },
+    
     delete: function (req, res) {
         let productIdd = req.params.id;
     
@@ -205,9 +150,17 @@ update: (req, res) => {
         
     },
 
-    productedit : (req,res)=>{
-        const products = productModel.all();
-        res.render('products/productedit', {products, toThousand})
+    productedit : async (req,res) => {
+        try {
+            const images = await db.Image.findAll();
+            const products =  await db.Product.findAll(
+                {include: [db.Image]
+            });
+            res.render('products/productedit', {products,images,toThousand})
+        } catch (error) {
+            res.json({error: error.message});
+        }
+        
     },
 
     filter: async (req,res) => {
