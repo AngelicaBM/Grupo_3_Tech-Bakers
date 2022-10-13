0e0dd6602fd164c9f0b7e980de3190647027ff71
Ezeiac <ezearena@gmail.com>
2022-10-12 22:24:35 -0300

    heroku

--- src/dataBase/models/index.js
+++ src/dataBase/models/index.js
@@ -10,7 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[process.env.JAWSDB_MARIA_URL], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
