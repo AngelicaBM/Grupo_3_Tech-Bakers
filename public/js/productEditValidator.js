1d7029d6e402cf42c751c0d68adf891c48eb2ebf
Chino89 <im.emm@live.com>
2022-10-29 12:07:47 -0300

    correcciones en formulario de Edicion de producto

--- public/js/productEditValidator.js
+++ public/js/productEditValidator.js
@@ -1,158 +1,173 @@
-window.onload = function () {
+window.addEventListener("load", function () {
+	// capturamos el form de Creacion de Producto
 
-  // capturamos los input a validar
-  const editName = document.querySelector("#name");
-  const editType = document.querySelector(".editType");
-  const editPrice = document.querySelector("#price");
-  const editDiscount = document.querySelector("#discount");
-  const editAvatar = document.querySelector("#image");
-  const editDescription = document.querySelector("#description");
-  const editStock = document.querySelector("#stock");
-  const editCategory = document.querySelector('[name="category"]');
+	const editForm = document.querySelector(".create_edit");
 
-  // capturamos los divs de validacion
-  const editNameError = document.querySelector("#editNameError");
-  const editTypeError = document.querySelector("#editTypeError");
-  const editPriceError = document.querySelector("#editPriceError");
-  const editDiscountError = document.querySelector("#editDiscountError");
-  const editAvatarError = document.querySelector("#editAvatarError");
-  const editDescriptionError = document.querySelector("#editDescriptionError");
-  const editStockError = document.querySelector("#editStockError");
-  const editCategoryError = document.querySelector("#editCategoryError");
+	// capturamos los input a validar
+	const editName = document.querySelector("#name");
+	const editType = document.querySelector(".editType");
+	const editPrice = document.querySelector("#price");
+	const editDiscount = document.querySelector("#discount");
+	const editAvatar = document.querySelector("#image");
+	const editDescription = document.querySelector("#description");
+	const editStock = document.querySelector("#stock");
+	const editCategory = document.querySelector('[name="category"]');
 
-  // Funciones de Validacion consumidas desde productCreateValidator.js
+	// capturamos los divs de validacion
+	const editNameError = document.querySelector("#editNameError");
+	const editTypeError = document.querySelector("#editTypeError");
+	const editPriceError = document.querySelector("#editPriceError");
+	const editDiscountError = document.querySelector("#editDiscountError");
+	const editAvatarError = document.querySelector("#editAvatarError");
+	const editDescriptionError = document.querySelector("#editDescriptionError");
+	const editStockError = document.querySelector("#editStockError");
+	const editCategoryError = document.querySelector("#editCategoryError");
 
-  const editNameValidator = () => {
-    let errors = "";
-    nameValue = editName.value.trim();
+	// Funciones de Validacion consumidas desde productCreateValidator.js
+	let fieldsWithErrors = {};
+	const editNameValidator = () => {
+		const errors = [];
+		nameValue = editName.value.trim();
 
-    if (!filled(nameValue)) {
-      errors = "Ingresa el Nombre del Producto";
-    } else if (productLength(nameValue)) {
-      errors = "El nombre debe tener al menos 5 caracteres";
-    }
+		if (!filled(nameValue)) {
+			errors.push("Ingresa el Nombre del Producto");
+		} else if (productLength(nameValue)) {
+			errors.push("El nombre debe tener al menos 5 caracteres");
+		}
 
-    errors = securityValidator(nameValue, errors);
-    editNameError.innerText = errors;
-  };
+		securityValidator(nameValue, errors, fieldsWithErrors);
+		buildErrorsText(editNameError, errors);
+		updateFieldsWithErrors(fieldsWithErrors, "editName", errors);
+	};
 
-  const editTypeValidator = () => {
-    let errors = "";
-    typeValue = editType.value;
+	const editTypeValidator = () => {
+		const errors = [];
+		typeValue = editType.value;
 
-    errors = selectValidator(typeValue, errors);
-    errors = securityValidator(typeValue, errors);
-    editTypeError.innerText = errors;
-  };
+		selectValidator(typeValue, errors, fieldsWithErrors);
+		securityValidator(typeValue, errors, fieldsWithErrors);
+		buildErrorsText(editTypeError, errors);
+		updateFieldsWithErrors(fieldsWithErrors, "editType", errors);
+	};
 
-  const editPriceValidator = () => {
-    let errors = "";
-    priceValue = editPrice.value.trim();
+	const editPriceValidator = () => {
+		const errors = [];
+		priceValue = editPrice.value.trim();
 
-    if (!filled(priceValue)) {
-      errors = "Ingresa el Precio del Producto";
-    } else if (priceValue < 0) {
-      errors = "El precio debe ser un número Positivo";
-    }
+		if (!filled(priceValue)) {
+			errors.push("Ingresa el Precio del Producto");
+		} else if (priceValue < 0) {
+			errors.push("El precio debe ser un número Positivo");
+		}
 
-    errors = securityValidator(priceValue, errors);
-    editPriceError.innerText = errors;
-  };
+		securityValidator(priceValue, errors, fieldsWithErrors);
+		buildErrorsText(editPriceError, errors);
+		updateFieldsWithErrors(fieldsWithErrors, "editPrice", errors);
+	};
 
-  const editDiscountValidator = () => {
-    let errors = "";
-    discountValue = editDiscount.value.trim();
+	const editDiscountValidator = () => {
+		const errors = [];
+		discountValue = editDiscount.value.trim();
 
-    if (!filled(discountValue)) {
-      errors = "El producto tiene algun Descuento?";
-    }
+		if (!filled(discountValue)) {
+			errors.push("El producto tiene algun Descuento?");
+		} else if (discountValue <= 0) {
+			errors.push("El descuento debe ser un número Positivo");
+		}
 
-    errors = securityValidator(discountValue, errors);
-    editDiscountError.innerText = errors;
-  };
+		securityValidator(discountValue, errors, fieldsWithErrors);
+		buildErrorsText(editDiscountError, errors);
+		updateFieldsWithErrors(fieldsWithErrors, "editDiscount", errors);
+	};
 
-  const editDescriptionValidator = () => {
-    let errors = "";
-    descriptionValue = editDescription.value.trim();
+	const editDescriptionValidator = () => {
+		const errors = [];
+		descriptionValue = editDescription.value.trim();
 
-    if (!filled(descriptionValue)) {
-      errors = "Ingresa la Descripción del Producto. Límite:  250 caracteres";
-    } else if (descriptionValue.length > 250) {
-      errors = "Superaste el límite de caracteres";
-    }
+		if (!filled(descriptionValue)) {
+			errors.push(
+				"Ingresa la Descripción del Producto. Límite:  250 caracteres"
+			);
+		} else if (descriptionValue.length > 250) {
+			errors.push("Superaste el límite de caracteres");
+		}
 
-    errors = securityValidator(descriptionValue, errors);
-    editDescriptionError.innerText = errors;
-  };
+		securityValidator(descriptionValue, errors, fieldsWithErrors);
+		buildErrorsText(editDescriptionError, errors);
+		updateFieldsWithErrors(fieldsWithErrors, "editDescription", errors);
+	};
 
-  const editStockValidator = () => {
-    let errors = "";
-    stockValue = editStock.value.trim();
+	const editStockValidator = () => {
+		const errors = [];
+		stockValue = editStock.value.trim();
 
-    if (!filled(stockValue)) {
-      errors = "Debes setear el Stock de éste Producto";
-    } else if (stockValue < 0) {
-      errors = "El stock debe ser un número Positivo";
-    } else if (stockValue >= 30) {
-      errors = "Alerta, stocks más limitados garantizan frescura";
-    }
+		if (!filled(stockValue)) {
+			errors.push("Debes setear el Stock de éste Producto");
+		} else if (stockValue < 0) {
+			errors.push("El stock debe ser un número Positivo");
+		} else if (stockValue >= 30) {
+			errors.push("Alerta, stocks más limitados garantizan frescura");
+		}
 
-    errors = securityValidator(stockValue, errors);
-    editStockError.innerText = errors;
-  };
+		securityValidator(stockValue, errors, fieldsWithErrors);
+		buildErrorsText(editStockError, errors);
+		updateFieldsWithErrors(fieldsWithErrors, "editStock", errors);
+	};
 
-  const editCategoryValidator = () => {
-    let errors = "";
-    categoryValue = editCategory.value;
+	const editCategoryValidator = () => {
+		const errors = [];
+		categoryValue = editCategory.value;
 
-    errors = selectValidator(typeValue, errors);
-    errors = securityValidator(categoryValue, errors);
+		selectValidator(typeValue, errors, fieldsWithErrors);
+		securityValidator(categoryValue, errors, fieldsWithErrors);
+		buildErrorsText(editCategoryError, errors);
+		updateFieldsWithErrors(fieldsWithErrors, "editCategory", errors);
+	};
 
-    editCategoryError.innerText = errors;
-  };
+	// Hacemos el Prevent Default del Submit
+	editForm.addEventListener("submit", function (event) {
+		event.preventDefault();
+		avatarValidator(editAvatar, editAvatarError, fieldsWithErrors);
+		editNameValidator(editName, editNameError, fieldsWithErrors);
+		editTypeValidator(editType, editTypeError, fieldsWithErrors);
+		editPriceValidator(editPrice, editPriceError, fieldsWithErrors);
+		editDiscountValidator(editDiscount, editDiscountError, fieldsWithErrors);
+		editDescriptionValidator(
+			editDescription,
+			editDescriptionError,
+			fieldsWithErrors
+		);
+		editStockValidator(editCategory, editCategoryError, fieldsWithErrors);
+		editCategoryValidator(editCategory, editCategoryError, fieldsWithErrors);
 
-  // Hacemos el Prevent Default del Submit
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    avatarValidator(editAvatar, editAvatarError);
-    editNameValidator();
-    editTypeValidator();
-    editPriceValidator();
-    editDiscountValidator();
-    editDescriptionValidator();
-    editStockValidator();
-    editCategoryValidator();
+		if (!Object.keys(fieldsWithErrors).length) {
+			editForm.submit();
+		}
+	});
 
-    if (errors.length) {
-      event.preventDefault();
-    } else {
-      form.submit();
-    }
-  });
+	// validamos formularios en tiempo real
 
-  // validamos formularios en tiempo real
+	editAvatar.addEventListener("blur", (e) =>
+		avatarValidator(editAvatar, editAvatarError, fieldsWithErrors)
+	);
+	editName.addEventListener("blur", editNameValidator);
+	editType.addEventListener("blur", editTypeValidator);
+	editPrice.addEventListener("blur", editPriceValidator);
+	editDiscount.addEventListener("blur", editDiscountValidator);
+	editDescription.addEventListener("blur", editDescriptionValidator);
+	editStock.addEventListener("blur", editStockValidator);
+	editCategory.addEventListener("blur", editCategoryValidator);
 
-  editAvatar.addEventListener("blur", (e) =>
-    avatarValidator(editAvatar, editAvatarError)
-  );
-  editName.addEventListener("blur", editNameValidator);
-  editType.addEventListener("blur", editTypeValidator);
-  editPrice.addEventListener("blur", editPriceValidator);
-  editDiscount.addEventListener("blur", editDiscountValidator);
-  editDescription.addEventListener("blur", editDescriptionValidator);
-  editStock.addEventListener("blur", editStockValidator);
-  editCategory.addEventListener("blur", editCategoryValidator);
+	// validamos formularios cuando se genere un cambio
 
-  // validamos formularios cuando se genere un cambio
-
-  editAvatar.addEventListener("change", (e) =>
-    avatarValidator(editAvatar, editAvatarError)
-  );
-  editName.addEventListener("change", editNameValidator);
-  editType.addEventListener("change", editTypeValidator);
-  editPrice.addEventListener("change", editPriceValidator);
-  editDiscount.addEventListener("change", editDiscountValidator);
-  editDescription.addEventListener("change", editDescriptionValidator);
-  editStock.addEventListener("input", editStockValidator);
-  editCategory.addEventListener("change", editCategoryValidator);
-};
+	editAvatar.addEventListener("change", (e) =>
+		avatarValidator(editAvatar, editAvatarError, fieldsWithErrors)
+	);
+	editName.addEventListener("change", editNameValidator);
+	editType.addEventListener("change", editTypeValidator);
+	editPrice.addEventListener("change", editPriceValidator);
+	editDiscount.addEventListener("change", editDiscountValidator);
+	editDescription.addEventListener("change", editDescriptionValidator);
+	editStock.addEventListener("input", editStockValidator);
+	editCategory.addEventListener("change", editCategoryValidator);
+});
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,271 +1,273 @@
-const db = require('../dataBase/models');
-const fs = require('fs');
-const path = require('path');
-const { validationResult } = require('express-validator');
-const universalModel = require('../model/universalModel.js');
-const productModel = universalModel ('products');
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-const { where } = require('sequelize');
+const db = require("../dataBase/models");
+const fs = require("fs");
+const path = require("path");
+const { validationResult } = require("express-validator");
+const universalModel = require("../model/universalModel.js");
+const productModel = universalModel("products");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const { where } = require("sequelize");
 const { Op } = require("sequelize");
-const {Product, Image, Type, Category} = require('../dataBase/models')
+const { Product, Image, Type, Category } = require("../dataBase/models");
 
 const productController = {
-    productDetails : async (req,res)=>{
-        try {
-            const id = req.params.id;
-            const product = await db.Product.findByPk(id, {
-                include: [db.Image]
-            });    
-            const products =  await db.Product.findAll(
-                {include: [db.Image]
-            });
-			const destacados = products.filter(product => product.Category =! 0);
-            destacados.splice(4)
-            res.render('products/productDetails', {product, destacados, toThousand});
-        } catch (error) {
-            res.json({error: error.message});
-        }
-    },
+	productDetails: async (req, res) => {
+		try {
+			const id = req.params.id;
+			const product = await db.Product.findByPk(id, {
+				include: [db.Image],
+			});
+			const products = await db.Product.findAll({ include: [db.Image] });
+			const destacados = products.filter((product) => (product.Category = !0));
+			destacados.splice(4);
+			res.render("products/productDetails", {
+				product,
+				destacados,
+				toThousand,
+			});
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-    create : async (req,res) => {
-        try {
-            const categories = await db.Category.findAll();
-            const types = await db.Type.findAll();
-            res.render('products/create', {categories,types, page_name: 'create'})
-        } catch (error) {
-            res.json({error: error.message});
-        }
-    },
+	create: async (req, res) => {
+		try {
+			const categories = await db.Category.findAll();
+			const types = await db.Type.findAll();
+			res.render("products/create", { categories, types, page_name: "create" });
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-    store: async (req, res) => {
-        try {
-            let product = req.body;
+	store: async (req, res) => {
+		try {
+			let product = req.body;
 
-                const errores = validationResult(req);
-            if (errores.isEmpty()) {
-                let imagenes= []
-                const productId = await db.Product.create(product);
-                for(let i = 0 ; i<req.files.length;i++) {
-                    imagenes.push({
-                        fileName: req.files[i].filename,
-                        productId: productId.id
-                    })
-                }
-                if (imagenes.length > 0) {
-                    await db.Image.bulkCreate(imagenes)
-                    res.redirect('/')
-                } else {
-                    await db.Image.create([{
-                        fileName: 'default-image.png',
-                        productId: productId,
-                    }])
-                    res.redirect('/')
-                }
-                                
-            } else {
-                if (req.files) {
-                    let {files} = req;
-                for (let i = 0 ; i< files.length; i++) {
-                    fs.unlinkSync(path.resolve(__dirname, '../../public/images/products/'+files[i].filename))
-                }
-                };
-                const categories = await db.Category.findAll();
-                const types = await db.Type.findAll();
-                res.render('products/create',{errors: errores.mapped(), oldData: req.body,types,categories});
-            }
-        } catch (error) {
-            res.json({error: error.message});
-        }
-        
-    },
-    
-   edit : async (req,res)=>{
-    try {
-        const categories = await db.Category.findAll();
-        const types = await db.Type.findAll();
-        const idToUpdate = +req.params.id;
-        const product = await db.Product.findByPk(idToUpdate,{
-            include: [db.Image,db.Category,db.Type]
-        });    
-        res.render('products/edit',{product, idToUpdate,categories,types, page_name: 'edit'});
-    } catch (error) {
-        res.json({error: error.message});
-    }
-    
-},
+			const errores = validationResult(req);
+			if (errores.isEmpty()) {
+				let imagenes = [];
+				const productId = await db.Product.create(product);
+				for (let i = 0; i < req.files.length; i++) {
+					imagenes.push({
+						fileName: req.files[i].filename,
+						productId: productId.id,
+					});
+				}
+				if (imagenes.length > 0) {
+					await db.Image.bulkCreate(imagenes);
+					res.redirect("/");
+				} else {
+					await db.Image.create([
+						{
+							fileName: "default-image.png",
+							productId: productId,
+						},
+					]);
+					res.redirect("/");
+				}
+			} else {
+				if (req.files) {
+					let { files } = req;
+					for (let i = 0; i < files.length; i++) {
+						fs.unlinkSync(
+							path.resolve(
+								__dirname,
+								"../../public/images/products/" + files[i].filename
+							)
+						);
+					}
+				}
+				const categories = await db.Category.findAll();
+				const types = await db.Type.findAll();
+				res.render("products/create", {
+					errors: errores.mapped(),
+					oldData: req.body,
+					types,
+					categories,
+				});
+			}
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-update: (req, res) => {
+	edit: async (req, res) => {
+		try {
+			const categories = await db.Category.findAll();
+			const types = await db.Type.findAll();
+			const idToUpdate = Number(req.params.id);
+			const product = await db.Product.findByPk(idToUpdate, {
+				include: [db.Image, db.Category, db.Type],
+			});
+			res.render("products/edit", {
+				product,
+				idToUpdate,
+				categories,
+				types,
+				page_name: "edit",
+			});
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-        db.Product.update({
-            
-            name: req.body.name,
-            price: req.body.price,
-            description: req.body.descripcion,
-            discount: req.body.discount,
-            stock: req.body.stock,
-            categoryId: req.body.category,
-            typeId: req.body.type,
-        },{
-            where:{
-                id: req.params.id
-            }
-        });
-             res.redirect('/products')
-    },
-    
-    delete: function (req, res) {
-        let productIdd = req.params.id;
-    
-        db.Product.findByPk(productIdd, {
-          include: ["Images"],
-        }).then(() => {
-          db.Image.destroy({
-            where: {
-              productId: productIdd,
-            },
-          }).then(() => {
-            db.Product.destroy({
-              where: {
-                id: productIdd,
-              },
-            }).then(() => {
-              return res.redirect("/");
-            });
-          });
-        });
-      },
- 
-    products: async (req,res) => {
-        try {
-            const  include = ['Type','Category','Images']
-            const products =  await db.Product.findAll(
-                {include
-            });
-            res.render('products/products', {products,
-                toThousand,
-                page_name: 'productos' })
-        } catch (error) {
-            res.json({error: error.message});
-        }
-        
-    },
+	update: (req, res) => {
+		db.Product.update(
+			{
+				name: req.body.name,
+				price: req.body.price,
+				description: req.body.descripcion,
+				discount: req.body.discount,
+				stock: req.body.stock,
+				categoryId: req.body.category,
+				typeId: req.body.type,
+			},
+			{
+				where: {
+					id: req.params.id,
+				},
+			}
+		);
+		res.redirect("/products");
+	},
 
-    productedit : async (req,res) => {
-        try {
-            const images = await db.Image.findAll();
-            const products =  await db.Product.findAll(
-                {include: [db.Image]
-            });
-            res.render('products/productedit', {products,images,toThousand})
-        } catch (error) {
-            res.json({error: error.message});
-        }
-        
-    },
+	delete: function (req, res) {
+		let productIdd = req.params.id;
 
-    filter: async (req,res) => {
-        try {
-            let filter = req.query;
-            const products = await db.Product.findAll({
-                include: [db.Type,db.Image,db.Category]
-            })
-            res.render('products/products', {products,toThousand,filter})
-        } catch (error) {
-            res.json({error: error.message});
-        }
-        
-    },
+		db.Product.findByPk(productIdd, {
+			include: ["Images"],
+		}).then(() => {
+			db.Image.destroy({
+				where: {
+					productId: productIdd,
+				},
+			}).then(() => {
+				db.Product.destroy({
+					where: {
+						id: productIdd,
+					},
+				}).then(() => {
+					return res.redirect("/");
+				});
+			});
+		});
+	},
 
-    pasteleria : async (req,res) => {
-            try {
-                const  include = ['Type','Category','Images']
-                const allProducts =  await db.Product.findAll(
-                    {include
-                });
-                const products = allProducts.filter(i => i.typeId == 2);  
-                
-                res.render('products/products',{
-                    allProducts,
-                    products,
-                    toThousand,
-                
-                });
-            } catch (error) {
-                res.json({error: error.message})
-            }
-    },
+	products: async (req, res) => {
+		try {
+			const include = ["Type", "Category", "Images"];
+			const products = await db.Product.findAll({ include });
+			res.render("products/products", {
+				products,
+				toThousand,
+				page_name: "productos",
+			});
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-    masas : async (req,res) => {
-        try {
-            const  include = ['Type','Category','Images']
-            const allProducts =  await db.Product.findAll(
-                {include
-            });
-            const products = allProducts.filter(i => i.typeId == 3);
+	productedit: async (req, res) => {
+		try {
+			const images = await db.Image.findAll();
+			const products = await db.Product.findAll({ include: [db.Image] });
+			res.render("products/productedit", { products, images, toThousand });
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-            res.render('products/products',{
-                allProducts,
-                products,
-                toThousand,              
-            });
-        } catch (error) {
-            res.json({error: error.message})
-        }
-},
+	filter: async (req, res) => {
+		try {
+			let filter = req.query;
+			const products = await db.Product.findAll({
+				include: [db.Type, db.Image, db.Category],
+			});
+			res.render("products/products", { products, toThousand, filter });
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-    tortas : async (req,res) => {
-        try {
-            const  include = ['Type','Category','Images']
-            const allProducts =  await db.Product.findAll(
-                {include
-            });
-            const products = allProducts.filter(i => i.typeId == 1);
+	pasteleria: async (req, res) => {
+		try {
+			const include = ["Type", "Category", "Images"];
+			const allProducts = await db.Product.findAll({ include });
+			const products = allProducts.filter((i) => i.typeId == 2);
 
-            res.render('products/products',{
-                allProducts,
-                products,
-                toThousand,
-            
-            });
-        } catch (error) {
-            res.json({error: error.message})
-        }
-    },
+			res.render("products/products", {
+				allProducts,
+				products,
+				toThousand,
+			});
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-    search: async (req, res) => {
-        try {  
-            const  include = ['Type','Category','Images']
+	masas: async (req, res) => {
+		try {
+			const include = ["Type", "Category", "Images"];
+			const allProducts = await db.Product.findAll({ include });
+			const products = allProducts.filter((i) => i.typeId == 3);
 
-            
-            let products = await Product.findAll({
-                where: {
-                    name: {[Op.like] : '%' + req.query.search + '%'}
-                },
-                include
-            })
-            
-            res.render('./products/productSearch', {products,toThousand})
-        } catch (error) {
-            res.json(error)
-        }
-        
-    },
+			res.render("products/products", {
+				allProducts,
+				products,
+				toThousand,
+			});
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-    productCart: async (req, res) => {
-        try {
-            const order = await db.Sale.findAll({
-                where: {
-                    userId: req.session.userLogged.id,            
-                },
-                include: ["product"]
-            });
+	tortas: async (req, res) => {
+		try {
+			const include = ["Type", "Category", "Images"];
+			const allProducts = await db.Product.findAll({ include });
+			const products = allProducts.filter((i) => i.typeId == 1);
 
-            res.render("products/productCart", {order, toThousand});
-        } catch (error) {
-            res.json(error)
-        }
-    },
+			res.render("products/products", {
+				allProducts,
+				products,
+				toThousand,
+			});
+		} catch (error) {
+			res.json({ error: error.message });
+		}
+	},
 
-}
+	search: async (req, res) => {
+		try {
+			const include = ["Type", "Category", "Images"];
+
+			let products = await Product.findAll({
+				where: {
+					name: { [Op.like]: "%" + req.query.search + "%" },
+				},
+				include,
+			});
+
+			res.render("./products/productSearch", { products, toThousand });
+		} catch (error) {
+			res.json(error);
+		}
+	},
+
+	productCart: async (req, res) => {
+		try {
+			const order = await db.Sale.findAll({
+				where: {
+					userId: req.session.userLogged.id,
+				},
+				include: ["product"],
+			});
+
+			res.render("products/productCart", { order, toThousand });
+		} catch (error) {
+			res.json(error);
+		}
+	},
+};
 
 module.exports = productController;
